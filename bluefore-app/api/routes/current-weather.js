203c0046e4189556e0e33a68eed0c94611e444f6
DavidSandor <sandor.david89@gmail.com>
2020-10-26 12:01:27 +0100

    Current weather route - common function for getting the response

--- bluefore-app/api/routes/current-weather.js
+++ bluefore-app/api/routes/current-weather.js
@@ -10,22 +10,33 @@ router.get('/api/current-weather/:lat/:lon', async (req, res, next) => {
     const latitude = req.params.lat;
     const longitude = req.params.lon;
 
-    //Query params
+    // Query params
     const language = req.query.lang;
 
-    const currentWeather = (await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=${language}&appid=${environment.WEATHER_API_KEY}`)).data;
-    res.json(new CurrentWeather(currentWeather));
+    res.json(await getCurrentWeatherResponse({latitude, longitude, language}));
 });
 
 router.get('/api/current-weather/:loc', async (req, res, next) => {
     // Route params
     const location = req.params.loc;
 
-    //Query params
+    // Query params
     const language = req.query.lang;
 
-    const currentWeather = (await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=${language}&appid=${environment.WEATHER_API_KEY}`)).data;
-    res.json(new CurrentWeather(currentWeather));
+    res.json(await getCurrentWeatherResponse({location, language}));
 });
+
+const getCurrentWeatherResponse = async (args) => {
+    try {
+        const currentWeather = (
+            await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args.location || ''}&lat=${args.latitude || ''}&lon=${args.longitude || ''}&units=metric&lang=${args.language || ''}&appid=${environment.WEATHER_API_KEY}`)
+            ).data; 
+
+        return Promise.resolve(new CurrentWeather(currentWeather));
+        
+    } catch (error) {
+        return {msg: 'Request failed!'};
+    }
+}
 
 module.exports = router;
