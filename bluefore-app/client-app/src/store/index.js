6a22b19525c2c666595eb2102690a32c369772fb
DavidSandor <sandor.david89@gmail.com>
2020-10-26 21:29:54 +0100

    Vuex store init for current weather

--- bluefore-app/client-app/src/store/index.js
+++ bluefore-app/client-app/src/store/index.js
@@ -2,8 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    location: '',
+    coordinates: '',
+    currentWeather: ''
+  },
+  getters: {
+    location(state) {
+      return state.location;
+    },
+    coordinates(state) {
+      return state.coordinates;
+    },
+    currentWeather(state) {
+      return state.currentWeather;
+    }
   },
   mutations: {
+    setCurrentWeather(state, currentWeather) {
+      state.currentWeather = {...currentWeather};
+      state.location = currentWeather.location;
+      state.coordinates = {...currentWeather.coordinates};
+    }
   },
   actions: {
   },
