364ba4864975383807ab0b68c367c4898cbbac11
DavidSandor <sandor.david89@gmail.com>
2020-10-27 20:19:48 +0100

    Date helper wekkdays fix

--- bluefore-app/client-app/src/utility/date-helper.js
+++ bluefore-app/client-app/src/utility/date-helper.js
@@ -1,16 +1,16 @@
 export default {
     getWeekday(index) {
         const days = [
+            'Sunday',
             'Monday',
             'Tuesday',
             'Wednesday',
             'Thursday',
             'Friday',
             'Saturday',
-            'Sunday'
         ]
 
-        return days[index-1];
+        return days[index];
     },
     convertToHourFormat(date) {
         return `${date.getUTCHours() > 9 ? date.getUTCHours() : '0' + date.getUTCHours()} : ${date.getUTCMinutes() > 9 ? date.getUTCMinutes() : '0' + date.getUTCMinutes()}`;
