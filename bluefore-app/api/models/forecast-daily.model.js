3912370ce1dd0b957e7e778e05f20bf8dd0f75f1
DavidSandor <sandor.david89@gmail.com>
2020-10-27 12:37:42 +0100

    Forecast model changes: time + timezone

--- bluefore-app/api/models/forecast-daily.model.js
+++ bluefore-app/api/models/forecast-daily.model.js
@@ -8,11 +8,10 @@ class ForecastDaily {
     description;
 
     constructor(forecastWeatherData) {
-        this.dateTime = forecastWeatherData.dt;
-        this.sunrise = forecastWeatherData.sunrise;
-        this.sunset = forecastWeatherData.sunset;
+        this.dateTime = forecastWeatherData.dt * 1000;
+        this.sunrise = forecastWeatherData.sunrise * 1000;
+        this.sunset = forecastWeatherData.sunset * 1000;
         this.temperature = forecastWeatherData.temp;
-        this.dateTime = forecastWeatherData.dt;
         this.iconId = forecastWeatherData.weather[0].id;
         this.description = forecastWeatherData.weather[0].description;
     }
