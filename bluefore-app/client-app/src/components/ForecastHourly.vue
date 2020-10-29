e3f2e437117e64f1ea3d535272a54355981a2e2b
DavidSandor <sandor.david89@gmail.com>
2020-10-29 15:06:34 +0100

    Hourly forecast frontend updates + drops.svg

--- bluefore-app/client-app/src/components/ForecastHourly.vue
+++ bluefore-app/client-app/src/components/ForecastHourly.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        <h2>Hourly forecast</h2>
-        <div class="hourly-container">
-            <div class="forecast" v-for="forecast in forecastHourly" :key="forecast.dateTime">
-                <p>{{convertToday(forecast.dateTime + forecast.timezoneOffset)}}</p>
-                <p>{{convertHour(forecast.dateTime + forecast.timezoneOffset)}}</p>
-                <p>{{forecast.description}}</p>
-                <i class="fh-icon" :class="`owi owi-${forecast.iconId}`"></i>
-                <p>{{toDegreeFormat(forecast.temperature)}}</p>
+    <div class="hourly-weather-panel">
+        <div class="hourly-weather" v-for="forecast in forecastHourly" :key="forecast.dateTime">
+            <p class="hw-day">{{convertToday(forecast.dateTime + forecast.timezoneOffset)}}</p>
+            <p class="hw-hour">{{convertHour(forecast.dateTime + forecast.timezoneOffset)}}</p>
+            <i class="hw-icon" :class="`owi owi-${forecast.iconId}`"></i>
+            <p class="hw-precipitation"><img src="../assets/icons/drops.svg" />{{(forecast.precipitationProbability * 100).toFixed(0)}}%</p>
+            <div class="hw-temperature-meter">
+                <div class="inner-meter" :style="{height: `${getTemperatureMeterValue(forecast.temperature)}%`}">
+                    <p class="hw-temperature">{{toDegreeFormat(forecast.temperature)}}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -20,6 +21,12 @@ import WEATHER_HELPER from '@/utility/weather-helper';
 import { mapGetters } from 'vuex';
 
 export default {
+    data() {
+        return {
+            maxTemp: -100,
+            minTemp: 100
+        }
+    },
     computed: {
     ...mapGetters([
         'forecastHourly'
@@ -32,25 +39,111 @@ export default {
         convertToday(date) {
             return (new Date(date)).getUTCDate() > (new Date().getUTCDate()) ? 'Tomorrow' : 'Today';
         },
-        toDegreeFormat(value) {
-            return WEATHER_HELPER.toDegreeFormat(value);
+        toDegreeFormat(deg) {
+            return WEATHER_HELPER.toDegreeFormat(deg);
+        },
+        getTemperatureMeterValue(temp) {
+            const percentage = (temp / this.maxTemp * 100).toFixed(2);
+
+            return percentage < 20 ? 20 : percentage;
+        },
+        setMinMaxTemp(forecastHourly) {
+            this.maxTemp = -100;
+            this.minTemp = 100;
+
+            for (const key in forecastHourly) {
+                if (forecastHourly.hasOwnProperty(key)) {
+                    const forecast = forecastHourly[key];
+                    if(forecast.temperature > this.maxTemp) {
+                        this.maxTemp = forecast.temperature;
+                    }
+
+                    if(forecast.temperature < this.minTemp) {
+                        this.minTemp = forecast.temperature;
+                    }
+                }
+            }
+        }
+    },
+    watch: {
+        forecastHourly(forecast) {
+            this.setMinMaxTemp(forecast);
         }
     }
 }
 </script>
 
-<style scoped>
-.hourly-container {
+<style scoped lang="scss">
+.hourly-weather-panel {
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
 }
 
-.forecast {
-    border: 1px dashed green;
-    margin: 5px;
+.hourly-weather {
+    width: 84px;
+    background-color: white;
+    border-radius: 30px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    margin-left: 20px;
+    text-align: center;
+    padding-top: 20px;
+    position: relative;
+
+    p {
+        margin-bottom: 10px;
+    }
+
+    .hw-day {
+        font-size: 14px;
+        color: #848484;
+    }
+
+    .hw-hour {
+        font-size: 14px;
+    }
+
+    .hw-icon {
+        font-size: 40px;
+        color:#7D7D7D;
+    }
+
+    .hw-precipitation {
+        font-size: 14px;
+        color: #848484;
+        margin-top: 10px;
+
+        img {
+            height: 16px;
+            margin-right: 3px;
+            margin-bottom: 5px;
+        }
+    }
+
+    .hw-temperature-meter {
+        position: absolute;
+        left: 10px;
+        right: 10px;
+        bottom: 10px;
+        top: 170px;
+        border-radius: 25px;  
+
+        .inner-meter {      
+            border-radius: inherit;
+            padding-top: 10px;
+            background-color: rgba($color: #0488F6, $alpha: 0.25);
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+
+            .hw-temperature {
+                font-size: 16px;
+                font-weight: bold;
+            }
+        }
+    }
 }
 
-.fh-icon {
-    font-size: 40px;
-}
+
 </style>
